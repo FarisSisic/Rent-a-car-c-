774be1dfaa2b7536f8e54b31696537f617912b5c
Faris Šišić <faris.sisic.personal@gmail.com>
2023-04-10 15:09:08 +0200

    Update strukture.h
    Pocetak struktura otprilike po cemu cemo raditi.

---
+++ header/strukture.h
@@ -0,0 +1,27 @@
+struct Auta{
+	int id_auta;
+	char naziv_auta;
+	int sjedista;
+	int cijena_po_danu;
+	char gorivo;
+	char pogon;
+};
+struct Transmisija{
+	Auta auta;
+	char transmisija;
+};
+struct Pogon{
+	Transmisija trans;
+	char pogon;
+};
+
+struct Kubikaza{
+	Pogon pogon;
+	int kubikaza;
+	char gorivo;
+};
+struct Datum{
+	int d,m,g;
+};
+
+
