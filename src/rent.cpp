7829e4cec58a25e47e24ed901ea55f7f353b8d37
burosalih <117530059+burosalih@users.noreply.github.com>
2023-04-10 16:04:35 +0200

    Update rent.cpp

--- src/rent.cpp
+++ src/rent.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
     int opcija;
     cout << "---------------------------------------\n"<<"Dobrodosli u nasu Rent-A-Car agenciju !\n"<<"---------------------------------------\n";
-    cout << "Odaberite opciju :\n 1.Registracija\n 2.Login\n";
+    cout << "\nOdaberite opciju :\n 1.Registracija\n 2.Login\n";
     cin >> opcija;
     if(opcija == 1){registracija();cout <<"\n";login();}
     if(opcija == 2){login();}
