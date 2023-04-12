ec86806520e43196db9400a442675dd33a6dd019
Faris Šišić <faris.sisic.personal@gmail.com>
2023-04-12 20:34:21 +0200

    Update rent.cpp

--- src/rent.cpp
+++ src/rent.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include "../header/funkcije.h"
-#include "../header/strukture.h"
+//#include "../header/strukture.h"
 using namespace std;
 
+
 int main(){
+	Korisnici k;
     int opcija;
     cout << "---------------------------------------\n"<<"Dobrodosli u nasu Rent-A-Car agenciju !\n"<<"---------------------------------------\n";
     cout << "\nOdaberite opciju :\n 1.Registracija\n 2.Login\n";
     cin >> opcija;
-    if(opcija == 1){registracija();cout <<"\n";login();}
-    if(opcija == 2){login();}
+    if(opcija == 1){registracija(k);cout <<"\n";login(k);}
+    if(opcija == 2){login(k);}
     system("pause");
     return 0;
 }
