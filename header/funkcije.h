12b458de62018849ff1ca787bd2f10a4dc416b85
Faris Šišić <faris.sisic.personal@gmail.com>
2023-04-12 20:34:16 +0200

    Update funkcije.h

--- header/funkcije.h
+++ header/funkcije.h
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include "strukture.h"
 using namespace std;
 void unosDatuma(int d, int m, int g){
 	int mjesec[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
@@ -29,40 +30,42 @@ void unosDatuma(int d, int m, int g){
 	}
 	}while(g<2023);
 }
-void registracija() {
-   string username, password, Ime, Prezime;
 
+
+void registracija(Korisnici k) {
+   //string username, password, Ime, Prezime;
+   //Korisnici k;
    cout << "Unesite svoje ime: "; 
-   cin >> Ime;
+   cin >> k.ime;
 
    cout << "Unesite svoje prezime: ";
-   cin >> Prezime;
+   cin >> k.prezime;
 
    cout << "Unesite vlastito korisnicko ime: ";
-   cin >> username;
+   cin >> k.username;
 
    cout << "Unesite sifru: ";
-   cin >> password;
+   cin >> k.password;
 
    ofstream outFile;
    outFile.open("korisnici.txt", ios::app); // Otvaranje fajla u append modu da bi se novi korisnici dodavali na kraj fajla
-   outFile << Ime << " " << Prezime << " " << username << " " << password <<"\n" << endl;
+   outFile << k.ime << " " << k.prezime << " " << k.username << " " << k.password <<"\n" << endl;
    outFile.close();
    
    cout << "Vasa registracija je uspjesna ! Sada se mozete loginovati." << endl;
 }
-void login() {
-   string username, password, storedUsername, storedPassword, Ime, Prezime; //postavljamo varijable storedUsername i storedPassword da bi mogli provjeriti da li se uneseni podaci i podaci u .txtu podudaraju
+void login(Korisnici k) {
+   string storedUsername, storedPassword; //postavljamo varijable storedUsername i storedPassword da bi mogli provjeriti da li se uneseni podaci i podaci u .txtu podudaraju
    bool found = false; // varijabla found se koristi da provjerimo da li postoji korisnik
    cout << "Unesite svoje korisnicko ime: ";
-   cin >> username;
+   cin >> k.username;
    cout << "\nUnesite sifru: ";
-   cin >> password;
+   cin >> k.username;
    
    ifstream inFile; // provjeravamo da li postoji korisnik
    inFile.open("korisnici.txt");
-   while (inFile >> Ime >> Prezime >> storedUsername >> storedPassword) {
-      if (username == storedUsername && password == storedPassword) {
+   while (inFile >> k.ime >> k.prezime >> storedUsername >> storedPassword) {
+      if (k.username == storedUsername && k.password == storedPassword) {
          found = true;
          break;
       }
@@ -70,11 +73,11 @@ void login() {
    inFile.close();
    
    if (found) {
-      if (username == "admin" && password == "adminadmin") {
+      if (k.username == "admin" && k.password == "adminadmin") {
          cout << "Administrator ulogovan !" << endl;
       } 
       else {
-         cout << "Dobrodosli, " << Ime << " " << Prezime << "!" << endl;
+         cout << "Dobrodosli, " << k.ime << " " << k.prezime << "!" << endl;
       }
    } else {
       cout << "Neispravan unos." << endl;
